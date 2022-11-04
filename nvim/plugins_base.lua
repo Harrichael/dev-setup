e6f8e597208f830dc8f7b9a69ed04f44f1fc7050
Harrichael <mtharrington@google.com>
2022-11-04 00:20:54 -0700

    Better auto plugin

--- nvim/plugins_base.lua
+++ nvim/plugins_base.lua
@@ -1,15 +1,26 @@
 
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  print("Downloading wbthomason/packer.nvim...")
-  local install_cmd = "git clone https://github.com/wbthomason/packer.nvim --depth 1 " .. install_path
-  local stdout = vim.fn.system(install_cmd)
-  print(stdout)
-  print("Packer installed. You'll need to restart now.")
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    print("Downloading wbthomason/packer.nvim...")
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    print("Packer installed")
+    return true
+  end
+  return false
 end
+local packer_bootstrap = ensure_packer()
 
 -- :PackerUpdate
 -- :PackerSync
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins_base.lua source <afile> | PackerSync
+  augroup end
+]])
 
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
@@ -39,5 +50,10 @@ require('packer').startup(function(use)
   --   <character-surround-to-replace>
   --   <character-to-surround-with>
   use 'tpope/vim-surround'
+
+  
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 
 end)
