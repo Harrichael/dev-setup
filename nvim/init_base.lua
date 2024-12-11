ccb3f3e68fcc81b5e51a4eb5de15a1863b795c91
Michael Harrington <michaeltoddharrington@gmail.com>
2024-12-11 14:33:07 -0700

    Neovim - No auto change directory

--- nvim/init_base.lua
+++ nvim/init_base.lua
@@ -1,10 +1,10 @@
 
 
 -- Searching
-vim.o.ignorecase  = true            -- case insensitive 
-vim.o.smartcase   = true            -- requires ignorecase, but capitals in searchs are not ignored
-vim.o.hlsearch    = true            -- highlight search 
-vim.g.noincsearch = true            -- incremental search is annoying
+vim.o.ignorecase  = true           -- case insensitive 
+vim.o.smartcase   = true           -- requires ignorecase, but capitals in searchs are not ignored
+vim.o.hlsearch    = true           -- highlight search 
+vim.g.noincsearch = true           -- incremental search is annoying
 
 -- Indentation
 vim.o.autoindent  = true           -- indent a new line the same amount as the line just typed
@@ -19,8 +19,9 @@ vim.o.showmatch   = true           -- show matching parens
 
 -- Usability
 vim.o.ttyfast     = true           -- Speed up scrolling in Vim
-vim.o.history     = 10000           -- Elephants never forget
+vim.o.history     = 10000          -- Elephants never forget
 vim.o.autoread    = true           -- If the file changes outside vim, automatically read it again
+vim.o.autochdir   = false          -- Don't auto change directory! Logic is busted, avoid. KISS.
 vim.o.scrolloff   = 5              -- Minimum spacing from current line to top or bottom
 vim.api.nvim_create_autocmd(       -- Saves the position of screen at exit 
   'BufWinLeave',
@@ -36,12 +37,12 @@ vim.api.nvim_create_autocmd(       -- Restores last screen position
 )
 
 -- Functional Behavior
-vim.o.wildmode    = 'longest,list'   -- get bash-like tab completions
-vim.o.clipboard   = 'unnamedplus'   -- using system clipboard
+vim.o.wildmode    = 'longest,list' -- get bash-like tab completions
+vim.o.clipboard   = 'unnamedplus'  -- using system clipboard
 
 -- Keymaps
-vim.g.mapleader = ' '               -- Use space to prefix our custom functions that won't collide with builtins
-vim.keymap.set('n', 'x', '"_x')     -- Use x key to delete characters without overwriting register
+vim.g.mapleader = ' '              -- Use space to prefix our custom functions that won't collide with builtins
+vim.keymap.set('n', 'x', '"_x')    -- Use x key to delete characters without overwriting register
 vim.keymap.set('n', '<leader>l', ':nohl<CR>')   -- Clear search highlights
 
 --Treesitter highlighting
