ad89b22a0f62269ae8af1e91b90603615b8fa2bb
Michael Harrington <michaeltoddharrington@gmail.com>
2024-12-13 17:10:11 -0700

    Setup treesitter highlighting for terraform

--- nvim/init_base.lua
+++ nvim/init_base.lua
@@ -6,16 +6,19 @@ vim.o.smartcase   = true           -- requires ignorecase, but capitals in searc
 vim.o.hlsearch    = true           -- highlight search 
 vim.g.noincsearch = true           -- incremental search is annoying
 
+
 -- Indentation
 vim.o.autoindent  = true           -- indent a new line the same amount as the line just typed
 vim.o.expandtab   = true           -- converts tabs to white space
 vim.o.shiftwidth  = 4              -- uses 4 spaces for a tab, used for auto indenting
 vim.o.tabstop     = 4              -- uses 4 spaces for a tab, used when hitting tab key explicitly
 
+
 -- Static Appearance
 vim.o.number      = true           -- add line numbers
 vim.o.cursorline  = true           -- highlight current cursorline
 vim.o.showmatch   = true           -- show matching parens
+
 
 -- Usability
 vim.o.ttyfast     = true           -- Speed up scrolling in Vim
@@ -36,19 +39,36 @@ vim.api.nvim_create_autocmd(       -- Restores last screen position
   }
 )
 
+
 -- Functional Behavior
 vim.o.wildmode    = 'longest,list' -- get bash-like tab completions
 vim.o.clipboard   = 'unnamedplus'  -- using system clipboard
+
 
 -- Keymaps
 vim.g.mapleader = ' '              -- Use space to prefix our custom functions that won't collide with builtins
 vim.keymap.set('n', 'x', '"_x')    -- Use x key to delete characters without overwriting register
 vim.keymap.set('n', '<leader>l', ':nohl<CR>')   -- Clear search highlights
 
---Treesitter highlighting
+
+--Treesitter config
 require'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
     },
 }
 
+local parsers = require'nvim-treesitter.parsers'
+local ensure_installed = { "hcl" }
+
+for _, lang in ipairs(ensure_installed) do
+  if not parsers.has_parser(lang) then
+    vim.cmd("TSInstall " .. lang)
+  end
+end
+
+vim.filetype.add({
+  extension = {
+    tf = "hcl",
+  },
+})
